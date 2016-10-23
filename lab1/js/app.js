c7d8f02748bb7580a2abc90579d3d31c4f2f85c1
Ryuichi001 <samed.sulanc@yahoo.de>
2016-10-24 00:58:30 +0200

    Video Funktionalitäten eingefügt

--- lab1/js/app.js
+++ lab1/js/app.js
@@ -16,13 +16,37 @@ window.addEventListener("load", function(event) {
 	var btnsPause = document.getElementsByClassName("btnPause");
 	var btnsStop = document.getElementsByClassName("btnStop");
 	
-		for (var i = 0; i < videos.length; i++) 
-		{
-			var video = videos[i];
-			var btnPlay = btnsPlay[i];
-			// … find button objects and add listener …
-			btnPlay.addEventListener("click", function (event) {
-				video.play();
-			}); // …
-	}
+for( var i = 0; i < videos.length; i++ ) {
+        btnsPlay[i].addEventListener('click', playListner.bind( null, i) );
+        btnsPause[i].addEventListener('click', pauseListner.bind( null, i) );
+        btnsStop[i].addEventListener('click', stopListner.bind( null, i) );
+}
+
+
+function playListner(index) {
+         videos[index].play();
+}
+
+function pauseListner(index) {
+         videos[index].pause();
+}
+
+function stopListner(index) {
+         videos[index].currentTime = 0;
+         videos[index].pause();
+}
+
+ 
+// eine andere Möglichkeit ist statt var i = 0, let i = 0 zu benutzen
+
+		// for (let i = 0; i < videos.length; i++) 
+		// {
+		// 	var video = videos[i];
+		// 	var btnPlay = btnsPlay[i];
+		// 	// … find button objects and add listener …
+		// 	btnPlay.addEventListener("click", function (event) {
+		// 	videos[i].play();
+
+		// }); 
+	    //}
 });
