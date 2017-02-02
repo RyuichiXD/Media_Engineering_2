dfb10884ea95cdcab2eb94c8fa225d1d9f4fe74e
Clemens <asdfsadf>
2017-02-02 10:55:38 +0100

    fixed small layout bug + add comments to methods

--- lab6/public/js/views/video-list.js
+++ lab6/public/js/views/video-list.js
@@ -2,22 +2,28 @@
  *  Backbone View (stub code) using the #xx-yy-zz-template from DOM to render ... into target element #xx-yy-target
  *  Needs model to be set from outside
  *
- *  (file can be deleted or changed für Ü6 videos)
+ *
  *
  *  @author Clemens Buckert
  */
 define(['backbone', 'jquery', 'underscore', 'views/video'], function (Backbone, $, _, VideoView) {
     var VideoListView = Backbone.View.extend({
+        //html root element
         el: '#video-list',
+        //we use videoView as template => Delegator View
         template: undefined,
         render: function () {
+            //empty the #video-list
             this.$el.empty();
+            //loop trough model.video data
             this.collection.each(function (video) {
                 var videoView = new VideoView({model: video});
+                //videoView.render() calls videoView itself, so we append $el to the DOM via jquery
                 this.$el.prepend(videoView.render().$el);
             }, this);
             return this;
         },
+        //Loop
         initialize: function () {
             this.listenTo(this.collection, 'add', this.render);
         }
--- lab6/public/js/views/video.js
+++ lab6/public/js/views/video.js
@@ -2,19 +2,22 @@
  *  Backbone View (stub code) using the #xx-yy-zz-template from DOM to render ... into target element #xx-yy-target
  *  Needs model to be set from outside
  *
- *  (file can be deleted or changed für Ü6 videos)
+ *
  *
  *  @author Clemens Buckert
  */
 define(['backbone', 'jquery', 'underscore'], function(Backbone, $, _) {
     var VideoView = Backbone.View.extend({
+        //html-tag
         tagName: "article",
+        //assign index.html script (<script type="text/template" id="video-template">) via _template to backbone template
         template : _.template($("#video-template").text()),
         render: function () {
-            //append model properties to the html: see http://backbonejs.org/#Model-attributes
+            //append model properties to the html template: see slides p. 74 or http://backbonejs.org/#Model-attributes
             this.$el.html(this.template(this.model.attributes));
             return this;
         },
+        //look for model changes and render template if
         initialize : function () {
             this.listenTo(this.model, "change", this.render);
         }
